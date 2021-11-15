b8ffc95d8bdab81047fd118aa90cb2d13cae06a4
LHThuat <lehoangthuat2002@gmail.com>
2021-11-15 16:27:50 +0700

    Update JdbcHelper.java

--- QuanLyKhachSan/src/com/exemple/helper/JdbcHelper.java
+++ QuanLyKhachSan/src/com/exemple/helper/JdbcHelper.java
@@ -18,65 +18,62 @@ import java.util.logging.Logger;
  * @author hp
  */
 public class JdbcHelper {
-
-    public static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyKhachSan";
-    public static String username = "sa";
-    public static String password = "Aa123456";
-
+    public static String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver"; 
+    public static String dburl="jdbc:sqlserver://localhost:1433;databaseName=QuanLyKhachSan";
+    public static String username="sa";
+    public static String password="admin";
+    
     //nạp driver
-    static {
+    static{
         try {
             Class.forName(driver);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(JdbcHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    public static PreparedStatement preparedStatement(String sql, Object... args) throws SQLException {
-        Connection con = DriverManager.getConnection(dburl, username, password);
-        PreparedStatement pstmt = null;
-        if (sql.startsWith("{")) {
-            pstmt = con.prepareCall(sql);
-        } else {
-            pstmt = con.prepareStatement(sql);
-        }
-        for (int i = 0; i < args.length; i++) {
-            pstmt.setObject(i + 1, args[i]);
+    
+ 
+    public static PreparedStatement preparedStatement(String sql,Object...args)throws SQLException{
+        Connection con=DriverManager.getConnection(dburl, username, password);
+        PreparedStatement pstmt=null;
+        if(sql.startsWith("{"))pstmt=con.prepareCall(sql); 
+        else pstmt=con.prepareStatement(sql);
+        for(int i=0; i<args.length;i++){
+            pstmt.setObject(i+1, args[i]);
         }
         return pstmt;
     }
-
-    public static void executeUpdate(String sql, Object... args) {
+    
+    public static void executeUpdate(String sql,Object...args){
         try {
-            PreparedStatement pstmt = JdbcHelper.preparedStatement(sql, args);
-            try {
+            PreparedStatement pstmt= JdbcHelper.preparedStatement(sql, args);
+            try{
                 pstmt.executeUpdate();
-            } finally {
-                pstmt.getConnection().close();
+            }finally{
+                pstmt.getConnection().close();      
             }
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
     }
-
-    public static ResultSet executeQuery(String sql, Object... args) throws SQLException {
-
-        PreparedStatement pstmt = JdbcHelper.preparedStatement(sql, args);
-        return pstmt.executeQuery();
+    
+    public static ResultSet executeQuery(String sql, Object...args) throws SQLException{
+       
+            PreparedStatement pstmt=JdbcHelper.preparedStatement(sql, args);
+                return pstmt.executeQuery();
     }
-
-    public static Object value(String sql, Object... args) {
+    
+    public static Object value(String sql, Object...args){
         try {
-            ResultSet rs = JdbcHelper.executeQuery(sql, args);
-            if (rs.next()) {
-                return rs.getObject(0);
-            }
-            rs.getStatement().getConnection().close();
-            return null;
+           ResultSet rs = JdbcHelper.executeQuery(sql, args);
+           if(rs.next()){
+               return rs.getObject(0);
+           }
+           rs.getStatement().getConnection().close();
+           return null;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
+        
     }
 }
