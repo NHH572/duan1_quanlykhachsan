296e669a6cea822251fa3397b865e28eb113ed5f
ppl1511 <zkiu912@gmail.com>
2021-11-15 10:47:47 +0700

    Update DangnhapJDialog.java

--- QuanLyKhachSan/src/com/exemple/views/DangnhapJDialog.java
+++ QuanLyKhachSan/src/com/exemple/views/DangnhapJDialog.java
@@ -5,6 +5,11 @@
  */
 package com.exemple.views;
 
+import com.exemple.controller.NhanVienDAO;
+import com.exemple.entity.NhanVien;
+import com.exemple.helper.Auth;
+import com.exemple.helper.MsgBox;
+
 /**
  *
  * @author Laptop
@@ -71,6 +76,11 @@ public class DangnhapJDialog extends javax.swing.JDialog {
 
         btnThoat.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btnThoat.setText("Thoát");
+        btnThoat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThoatActionPerformed(evt);
+            }
+        });
 
         btnDangnhap.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btnDangnhap.setText("Đăng nhập ");
@@ -186,7 +196,7 @@ public class DangnhapJDialog extends javax.swing.JDialog {
     }//GEN-LAST:event_txtTendangnhapActionPerformed
 
     private void btnDangnhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDangnhapActionPerformed
-        // TODO add your handling code here:
+    Login();
     }//GEN-LAST:event_btnDangnhapActionPerformed
 
     private void txtMatkhauActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMatkhauActionPerformed
@@ -198,6 +208,10 @@ public class DangnhapJDialog extends javax.swing.JDialog {
         // TODO add your handling code here:
  
     }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void btnThoatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThoatActionPerformed
+      exit();
+    }//GEN-LAST:event_btnThoatActionPerformed
 
     /**
      * @param args the command line arguments
@@ -224,6 +238,8 @@ public class DangnhapJDialog extends javax.swing.JDialog {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(DangnhapJDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
@@ -256,4 +272,33 @@ public class DangnhapJDialog extends javax.swing.JDialog {
     private javax.swing.JPasswordField txtMatkhau;
     private javax.swing.JTextField txtTendangnhap;
     // End of variables declaration//GEN-END:variables
-}
+NhanVienDAO dao = new NhanVienDAO();
+    private void Login() {
+        String manv = txtTendangnhap.getText();
+        String matKhau = new String(txtMatkhau.getPassword());
+        try {
+            NhanVien nhanVien = dao.selectById(manv);
+            if (nhanVien != null) {
+                String matKhau2 = nhanVien.getMatKhau();
+                if (matKhau.equals(matKhau2)) {
+                    Auth.user = nhanVien;
+                    MsgBox.alert(this, "Đăng nhập thành công!");
+                    this.dispose();
+                } else {
+                    MsgBox.alert(this, "Sai mật khẩu!");
+                }
+            } else {
+                MsgBox.alert(this, "Sai tên đăng nhập!");
+            }
+        } catch (Exception e) {
+            MsgBox.alert(this, "Lỗi truy vấn dữ liệu!");
+            e.printStackTrace();
+    }
+    }
+
+    private void exit() {
+         if (MsgBox.confirm(this, "Bạn có muốn thoát khỏi ứng dụng không?")) {
+            System.exit(0);
+    }
+    
+}}
