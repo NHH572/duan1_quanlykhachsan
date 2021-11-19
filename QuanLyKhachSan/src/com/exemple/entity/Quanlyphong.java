a26d631758c4c2d914d104d240f98cc75038022d
LHThuat <lehoangthuat2002@gmail.com>
2021-11-19 18:41:15 +0700

    Create Quanlyphong.java

---
+++ QuanLyKhachSan/src/com/exemple/entity/Quanlyphong.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.exemple.entity;
+
+/**
+ *
+ * @author ACER
+ */
+public class Quanlyphong {
+      private int maPhong;
+    private int soPhong;
+    private int Lau;
+    private String trangThai;
+    private int maLoaiPhong;
+
+    public int getMaPhong() {
+        return maPhong;
+    }
+
+    public void setMaPhong(int maPhong) {
+        this.maPhong = maPhong;
+    }
+
+    public int getSoPhong() {
+        return soPhong;
+    }
+
+    public void setSoPhong(int soPhong) {
+        this.soPhong = soPhong;
+    }
+
+    public int getLau() {
+        return Lau;
+    }
+
+    public void setLau(int Lau) {
+        this.Lau = Lau;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public int getMaLoaiPhong() {
+        return maLoaiPhong;
+    }
+
+    public void setMaLoaiPhong(int maLoaiPhong) {
+        this.maLoaiPhong = maLoaiPhong;
+    }
+
+    public String getTenLoaiPhong() {
+        return tenLoaiPhong;
+    }
+
+    public void setTenLoaiPhong(String tenLoaiPhong) {
+        this.tenLoaiPhong = tenLoaiPhong;
+    }
+
+    public int getDonGiaTheoNgay() {
+        return donGiaTheoNgay;
+    }
+
+    public void setDonGiaTheoNgay(int donGiaTheoNgay) {
+        this.donGiaTheoNgay = donGiaTheoNgay;
+    }
+
+    public int getDonGiaTheoGio() {
+        return donGiaTheoGio;
+    }
+
+    public void setDonGiaTheoGio(int donGiaTheoGio) {
+        this.donGiaTheoGio = donGiaTheoGio;
+    }
+
+    public int getGiaSauMotGio() {
+        return giaSauMotGio;
+    }
+
+    public void setGiaSauMotGio(int giaSauMotGio) {
+        this.giaSauMotGio = giaSauMotGio;
+    }
+
+    public int getMucTangCuoiTuan() {
+        return mucTangCuoiTuan;
+    }
+
+    public void setMucTangCuoiTuan(int mucTangCuoiTuan) {
+        this.mucTangCuoiTuan = mucTangCuoiTuan;
+    }
+
+    public int getMucTangNgayLe() {
+        return mucTangNgayLe;
+    }
+
+    public void setMucTangNgayLe(int mucTangNgayLe) {
+        this.mucTangNgayLe = mucTangNgayLe;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    private String tenLoaiPhong;
+    private int donGiaTheoNgay;
+    private int donGiaTheoGio;
+    private int giaSauMotGio;
+    private int mucTangCuoiTuan;
+    private int mucTangNgayLe;
+    private String moTa;
+    
+}
